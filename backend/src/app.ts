cb2923dab4256d23151c49324d338fbb9f5afa31
Ajit Vishwakarma <ajitvishwakarma202006@gmail.com>
2026-03-28 10:39:40 +0530

    Added cookie parser module

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./config/db";
 
 import authRouter from "./routes/auth.route"
+import { IUser } from "./lib/interfaces";
 
 // Configuring dotenv
 dotenv.config();
@@ -12,23 +14,19 @@ const app = express();
 
 // Middleware
 app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
-declare global{
-    namespace Express{
-        interface Request{
-            user:{
-                _id:string
-                email:string,
-                password:string,
-                name:string,
-            }
+declare global {
+    namespace Express {
+        interface Request {
+            user: IUser
         }
     }
 }
 
 // Mount routes
-app.use("/api/auth",authRouter);
+app.use("/api/auth", authRouter);
 
 const PORT = process.env.PORT || 5000;
 
