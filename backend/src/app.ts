f16e61da12398f6c6b86e8bb471d28ac3871b35e
Ajit Vishwakarma <ajitvishwakarma202006@gmail.com>
2026-03-28 10:02:01 +0530

    Created and added routes for auth functions

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,9 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import connectDB from './config/db';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db";
+
+import authRouter from "./routes/auth.route"
 
 // Configuring dotenv
 dotenv.config();
@@ -11,6 +13,22 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+declare global{
+    namespace Express{
+        interface Request{
+            user:{
+                _id:string
+                email:string,
+                password:string,
+                name:string,
+            }
+        }
+    }
+}
+
+// Mount routes
+app.use("/api/auth",authRouter);
 
 const PORT = process.env.PORT || 5000;
 
